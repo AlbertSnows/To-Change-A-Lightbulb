27ebb1de2a985d03de5b52f846ffa3b8ff9b8767
Albert Snow <ajsnow@ksu.edu>
2018-02-10 14:24:01 -0600

    Light Switch

---
+++ ToChangeALightbulb/Assets/Scripts/OnClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit didHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out didHit))
+            {
+                if (didHit.transform != null)
+                {
+                    GameObject objectInQuestion;
+                    if (objectInQuestion = didHit.transform.gameObject)
+                    {
+                        print(objectInQuestion.transform.gameObject.name);
+                        NotifyObject(objectInQuestion);
+                    }
+                }
+            }
+        }
+    }
+    private void NotifyObject(GameObject objectInQuestion)
+    {
+        objectInQuestion.SendMessage("Clicked"); 
+    }
+}
