0aaddec4ef0046f70d84c5c0c69da4cd783de272
Dayton Taylor <daytonltaylor@outlook.com>
2018-02-10 05:36:40 -0600

    removed unneccessary files and created main manu for Unity practice

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	private CharacterController charControl;
+	public float walkSpeed;
+
+	void Awake() {
+		charControl = GetComponent<CharacterController> ();
+	}
+
+	void Update() {
+		MovePlayer ();
+	}
+
+	void MovePlayer() {
+		float horiz = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		Vector3 moveDirSide = transform.right * horiz * walkSpeed;
+		Vector3 moveDirForward = transform.forward * vert * walkSpeed;
+
+		charControl.SimpleMove (moveDirSide);
+		charControl.SimpleMove (moveDirForward);
+	}
+}
