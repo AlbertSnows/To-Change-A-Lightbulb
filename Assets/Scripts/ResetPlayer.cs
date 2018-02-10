ad1c2636fa3e7844b8778f7151c365844949bb39
Dayton Taylor <daytonltaylor@outlook.com>
2018-02-10 17:14:38 -0600

    added respawn plane collision and player respawn script

---
+++ Assets/Scripts/ResetPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider obj) {
+		if(obj.gameObject.tag == "Reset Player Barrier") {
+			ResetPlayerPosition ();
+		}
+	}
+
+	private void ResetPlayerPosition() {
+		this.gameObject.transform.position = new Vector3(-12, 0, 0);
+	}
+}
