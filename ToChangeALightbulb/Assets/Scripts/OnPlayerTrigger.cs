99206bdc37f8c7882a2e2bf2585bc93e512fa87a
Albert Snow <ajsnow@ksu.edu>
2018-02-10 11:55:17 -0600

    Toggle Light Device

---
+++ ToChangeALightbulb/Assets/Scripts/OnPlayerTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlayerTrigger : MonoBehaviour {
+	public GameObject lightSource;
+    public Light lightProperty;
+
+    void Start()
+    {
+        lightProperty = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetButtonDown("Fire1")){
+		lightProperty.enabled = !lightProperty.enabled; 
+		}
+	}
+}
