495e99a1429c288e8c9459166b7ca7370c27c5be
Rayne Son <rson@ksu.edu>
2018-02-10 17:36:10 -0600

    Tried to change lightswitch... it didn't work. >:(

--- Assets/Scripts/OnPlayerTrigger.cs
+++ Assets/Scripts/OnPlayerTrigger.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class OnPlayerTrigger : MonoBehaviour {
 	public GameObject lightSource;
     public Light lightProperty;
-
+    
     void Start()
     {
         lightProperty = GetComponent<Light>();
+        
     }
 
     // Update is called once per frame
     void Update () {
-		if(Input.GetButtonDown("Fire1")){
-		lightProperty.enabled = !lightProperty.enabled; 
+		if(Input.GetButtonDown("Fire1"))
+        {
+		    lightProperty.enabled = !lightProperty.enabled; 
+            
 		}
 	}
 }
