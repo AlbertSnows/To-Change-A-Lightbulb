ba8db5a579c7979ffd177e091e53efe0e73caf54
Albert Snow <ajsnow@ksu.edu>
2018-02-10 15:23:30 -0600

    C4

---
+++ Assets/Scripts/LightBreaks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBreaks : MonoBehaviour {
+    Light fanLamp; 
+	// Use this for initialization
+	void Start () {
+        fanLamp = GetComponent<Light>();
+        StartCoroutine(BreakLightBulb());
+        fanLamp.enabled = false;
+    }
+    IEnumerator BreakLightBulb()
+    {
+        yield return new WaitForSecondsRealtime(5); 
+    }
+}
---
+++ Assets/Scripts/OnClickReceive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickReceive : MonoBehaviour {
+    public Light lightSwitch;
+    bool bulbBroken = false; 
+
+    void Start()
+    {
+        lightSwitch = GetComponent<Light>();
+    }
+    // Update is called once per frame
+    void Update () {
+	}
+
+    public void Clicked()
+    {
+        lightSwitch.enabled = !lightSwitch.enabled;
+        if(bulbBroken == false)
+        {
+            GameObject Ceiling_Fan = GameObject.Find("Ceiling_Fan");
+            Ceiling_Fan.SendMessage("BreakLightBulb"); 
+        }
+        bulbBroken = true; 
+
+    }
+}
