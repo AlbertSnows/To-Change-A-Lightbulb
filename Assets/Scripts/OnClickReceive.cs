d2027b7a7b195afe3345c850c36909c14b72994b
Albert Snow <ajsnow@ksu.edu>
2018-02-10 20:40:33 -0600

    Glow Stuff

--- Assets/Scripts/OnClickReceive.cs
+++ Assets/Scripts/OnClickReceive.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnClickReceive : MonoBehaviour {
-    public Light lightSwitch;
     bool bulbBroken = false; 
 
     void Start()
     {
-        lightSwitch = GetComponent<Light>();
     }
     // Update is called once per frame
     void Update () {
@@ -17,7 +15,6 @@ public class OnClickReceive : MonoBehaviour {
     public void Clicked()
     {
         print("Recieved!"); 
-        lightSwitch.enabled = !lightSwitch.enabled;
         if(bulbBroken == false)
         {
             GameObject Ceiling_Fan = GameObject.Find("Ceiling_Fan");
