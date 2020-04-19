0b39055230aef8f559491f73163f170330fb227b
AJ Snow <ajsnow@ksu.edu>
2020-04-19 17:59:54 -0500

    quick update to make things not broken
    being not broken is a good thing unless you're on reality TV

--- Assets/Scripts/ChangeSong.cs
+++ Assets/Scripts/ChangeSong.cs
@@ -8,18 +8,18 @@ public class ChangeSong : MonoBehaviour {
     AudioSource BGM; 
     public void Start()
     {
-        clip1 = GameObject.Find("Audio1").GetComponent<AudioClip>();
-        clip2 = GameObject.Find("Audio2").GetComponent<AudioClip>();
-        BGM = this.GetComponent<AudioSource>();
-        BGM.clip = clip1;
-        BGM.Play();
-        BGM.loop = true; 
+        // clip1 = GameObject.Find("Audio1").GetComponent<AudioClip>();
+        clip2 = GameObject.Find("Theme").GetComponent<AudioClip>();
+        // BGM = this.GetComponent<AudioSource>();
+        // BGM.clip = clip1;
+        // BGM.Play();
+        // BGM.loop = true; 
     }
     public void SwitchSong()
     {
-        print("Switching!"); 
-        BGM.Stop();
-        BGM.clip = clip2;
-        BGM.Play(); 
+        // print("Switching!"); 
+    // BGM.Stop();
+        // BGM.clip = clip2;
+        // BGM.Play(); 
     }
 }
--- Assets/Scripts/LightBreaks.cs
+++ Assets/Scripts/LightBreaks.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class LightBreaks : MonoBehaviour {
     GameObject player;
+    AudioSource currentSong;
+    AudioSource theme;
+    AudioSource stone;
     Light fanLamp; 
     bool gotItem = false; 
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("FirstPersonCharacter");
         fanLamp = GetComponent<Light>();
+        currentSong = GameObject.Find("Beginning").GetComponent<AudioSource>();
+        theme = GameObject.Find("Theme").GetComponent<AudioSource>();
+        stone = GameObject.Find("Staircase").GetComponent<AudioSource>();
     }
     IEnumerator BreakLightBulb()
     {
+        AudioSource lightBulbShattering = this.GetComponent<AudioSource>();
         fanLamp.enabled = true; 
         yield return new WaitForSecondsRealtime(5);
         fanLamp.enabled = false;
-        AudioSource lightBulbShattering = this.GetComponent<AudioSource>();
+        currentSong.Stop();
         lightBulbShattering.Play();
-        player.SendMessage("SwitchSong"); 
+        yield return new WaitForSecondsRealtime(lightBulbShattering.clip.length);
+        currentSong = theme;
+        currentSong.Play();
+        stone.Play();
+        // BroadcastMessage("SwitchSong");
+        // player.SendMessage("SwitchSong"); 
         BroadcastMessage("Appear"); 
     }
 }
